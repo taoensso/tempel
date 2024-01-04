dcacc741cfca263d7f17e1d29058652dfd23b0ed
Peter Taoussanis <peter@taoensso.com>
2024-01-04 10:20:01 +0100

    [nop] Update deps

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
    :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies
-  [[com.taoensso/encore "3.74.0"]]
+  [[com.taoensso/encore "3.76.0"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
--- src/taoensso/tempel.clj
+++ src/taoensso/tempel.clj
@@ -38,7 +38,7 @@
   (remove-ns 'taoensso.tempel)
   (:api (enc/interns-overview)))
 
-(enc/assert-min-encore-version [3 74 0])
+(enc/assert-min-encore-version [3 76 0])
 
 ;;;; TODO
 ;; - Consider including something like Signal's "Double Ratchet" work?
